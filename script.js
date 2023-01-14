120dc83c12b9050e567a7fcc31ae805a2377590e
Joshua Scott <scottjm@nsuok.edu>
2023-01-14 15:56:28 -0600

    Added size slider and comments.
    Changed original prompt for size to a slide, and added comments to the code.

--- script.js
+++ script.js
@@ -4,38 +4,56 @@ let click = false;
 
 
 document.addEventListener("DOMContentLoaded", function(){
-createBoard(100);
+createBoard(16);
 
+
+// gets color from color picker and sets it to variable
 colorPicker = document.querySelector('#color-picker');
 colorPicker.addEventListener('change', function() {
   console.log(colorPicker.value);
 })
-colorPicker.value = '#7b68ee';
 
 
 
+// get size from slider and updates board with that size.
+function setCurrentSize(newSize) {
+  currentSize = newSize
+}
+const sizeValue = document.getElementById('sizeValue')
+const sizeSlider = document.getElementById('sizeSlider')
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+function changeSize(value) {
+  setCurrentSize(value)
+  updateSizeValue(value)
+  reloadGrid()
+}
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} x ${value}`
+}
+function reloadGrid() {
+  createBoard(currentSize)
+}
+
+
+
+// allows for on and off clicking.
 document.querySelector("body").addEventListener("click",function(e){
     if (e.target.tagName != "BUTTON"){
         click=!click;
-        let draw = document.querySelector("#draw");
-        if(click){
-            draw.innerHtml = "Now you can draw"
-        }
-        else{
-            draw.innerHtml = "You're not allowed to draw. "
-        }
     }
 
 })
-let btn_popup = document.querySelector("#popup")
-btn_popup.addEventListener("click", function(){
+// let btn_popup = document.querySelector("#popup")
+// btn_popup.addEventListener("click", function(){
 
-    let size = input;
-    createBoard(size);
+    
+    
 })
 
-})
 
+// }}
 function createBoard(size){
     let board = document.querySelector(".board");
 
@@ -50,32 +68,16 @@ function createBoard(size){
         board.insertAdjacentElement("beforeend", div);
     }
 }
-function getSize(){
-    input = prompt("What will be the size of the board");
-    let message = document.querySelector("#message");
-    if (input ==""){
-        message.innerHTML ="Please provide a number";
-    }
-    else if(input < 0 || input > 100){
-        message.innerHTML = "Provide a number between 1 and 100"
-    }
-    else{
-        message.innerHTML = "Now you play!"
-        return input;
-    }
-}
 
 
-
-
+//function that changes color of pointer after button is pushed. 
 function colorDiv(){
     if (click){
-
         if(color == 'random'){
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
         }
-        else if(color == 'color-picker'){this.style.backgroundColor = colorPicker.value}
-     
+        else if(color == 'color-picker'){this.style.backgroundColor = colorPicker.value
+        }
         else{
         this.style.backgroundColor = 'black'
         }
@@ -84,7 +86,7 @@ function colorDiv(){
 function setColor(colorChoice){
      color = colorChoice;
 }
-
+// resets the etch a sketch board. 
 function resetBoard(){
     let divs = document.querySelectorAll("div")
     divs.forEach((div) => div.style.backgroundColor = "whitesmoke")
@@ -93,3 +95,19 @@ function resetBoard(){
 
 
 
+// old function for getting size from a prompt. using slider now. 
+
+// function getSize(){
+//     input = prompt("What will be the size of the board");
+//     let message = document.querySelector("#message");
+//     if (input ==""){
+//         message.innerHTML ="Please provide a number";
+//     }
+//     else if(input < 0 || input > 100){
+//         message.innerHTML = "Provide a number between 1 and 100"
+//     }
+//     else{
+//         message.innerHTML = "Now you play!"
+//         return input;
+//     }
+// }
